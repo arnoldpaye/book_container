a6bb640a8e9a1286ecffb8f5c19b39ff314ec199
Arnold Paye <arnoldpayet@gmail.com>
2015-06-14 12:47:25 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book.org Container</title>
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+</head>
+<body>
+    <h1>Book.org Container</h1>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "123456";
+        $database = "book_container";
+
+        // create connection
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        // check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn-> connect_error);
+        } else {
+            //echo "Connection successfully";
+            $query = "SELECT id,title,description,content,img FROM book";
+            if ($result = $conn->query($query)) {
+                echo '<ul class="wrapper columns-3">';
+                while($row = $result->fetch_assoc()) {
+                    echo '<li>';
+                    echo '<img src="./img/'.$row['img'].'"/>';
+                    echo '<h3>' . $row['title'] . '</h3>';
+                    echo '<p>' . $row['description'] . '</p>';
+                    echo '</li>';
+                }
+                echo '</ul>';
+            } else {
+                die("Connection failed: " . $conn-> connect_error);
+            }
+            $conn->close();
+        }
+
+    ?>
+</body>
+</html>
